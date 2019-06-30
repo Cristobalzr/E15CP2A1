12c017b1518668d2e72989e93689320607ad992a
Cristobal <cristobalz@outlook.com>
2019-06-30 18:04:35 -0400

    modifico vista de index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, :username, presence: true
+  validates_uniqueness_of :username
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :histories
